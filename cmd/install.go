69adfc23b1f3038c948681505282cafe2f003224
Souvik Haldar <souvik@Souviks-MacBook-Pro.local>
2019-10-29 01:49:08 +0530

    Node_exporter install code

--- cmd/install.go
+++ cmd/install.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
 	"runtime"
 
 	"github.com/spf13/cobra"
@@ -12,5 +13,9 @@ var install = &cobra.Command{
 	Short: "install node_exporter on the machine",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Installing node_Exporter on : ", runtime.GOOS)
+		c := exec.Command("sudo apt-get install prometheus-node-exporter")
+		if err := c.Run(); err != nil {
+			fmt.Println("Error in installing mustang: ", err)
+		}
 	},
 }
